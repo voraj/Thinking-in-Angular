b9eef6776721ec98ac2f024b3b9aebe4c0e96ba4
j0v0049 <jaini.vora@walmart.com>
2018-02-27 19:50:14 -0600

    Adding nested controllers

--- ngModel-Examples/app.js
+++ ngModel-Examples/app.js
@@ -1,11 +1,11 @@
-var app = angular.module("ngModelExamplesModule", []);
-app.controller("ExampleCtrlr", ExampleCtrl);
+var module = angular.module('nestedControllers', []);
+module.controller("Ctrl1", Ctrl1);
+module.controller("Ctrl2", Ctrl2);
 
-function ExampleCtrl($scope) {
-    $scope.textString = "null";
-    $scope.booleanValue = false;
+function Ctrl1() {
+    this.testScope = "From Ctrl 1"
+}
 
-    $scope.textChange = function () {
-        console.log("Text changed");
-    }
+function Ctrl2() {
+    this.testScope = "From Ctrl 2"
 }
