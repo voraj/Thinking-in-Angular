0cadc67cfbda93a01d3863c13c79a3c7c96f6233
j0v0049 <jaini.vora@walmart.com>
2018-03-02 19:17:23 -0600

    Added Edit and Delete functionality to To-do list

--- TodoApp/app.js
+++ TodoApp/app.js
@@ -5,12 +5,17 @@ app.controller("TodoController", todoCtrlr);
 function todoCtrlr() {
 
     this.todolist = ["Learn Angular", "Gym"];
-
+    this.editMode = false;
     this.addTodo = function () {
         this.todolist.push(this.todo);
-        console.log(this.todo);
         this.todo = "";
     }
 
+    this.deleteTodo = function (index) {
+        this.todolist.splice(index, 1);
+    }
 
+    this.triggerEditMode = function () {
+        this.editMode = !this.editMode;
+    }
 }
